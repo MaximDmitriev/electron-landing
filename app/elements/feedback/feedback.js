020cd847d9bf81e7aa4d6046e3640726f3a7c7af
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-06-13 23:35:16 +0300

    feedback result

--- app/elements/feedback/feedback.js
+++ app/elements/feedback/feedback.js
@@ -1,8 +1,8 @@
 const feedbackSlider = new Swiper(document.querySelector('.swiper-container[data-slider="feedback"]'), {
   slidesPerView: 1,
   loop: true,
-  // navigation: {
-  //   nextEl: '.slider__next_principles',
-  //   prevEl: '.slider__prev_principles',
-  // }
+  navigation: {
+    nextEl: '.slider__next_feedback',
+    prevEl: '.slider__prev_feedback',
+  }
 })
